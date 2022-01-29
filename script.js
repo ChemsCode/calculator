4e3c455603b08dba0cdaf9c7288cca133f4e461a
ChemsCode <saidiceb@gmail.com>
2022-01-28 21:38:32 -0500

    fully functioning calculator v1

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-function operate(operator, a, b){
+function operate(operator, b, a){
     switch(operator)
     {
         case "+":
-            calculatorDisplay.textContent = a + b;
+            calculatorDisplayBot.textContent = a + b;
+            return a+b;
         break;
         case "-":
-            calculatorDisplay.textContent = a - b;
+            calculatorDisplayBot.textContent = a - b;
+            return a-b;
         break;
         case "/":
-            calculatorDisplay.textContent = a / b;
+            calculatorDisplayBot.textContent = a / b;
+            return a/b;
         break;
         case "*":
-            calculatorDisplay.textContent = a * b;
+            calculatorDisplayBot.textContent = a * b;
+            return a*b;
+        break;
+        default:
+            return a;
         break;
     }
 }
@@ -20,13 +27,17 @@ function operate(operator, a, b){
 let x = 0;
 let y = 0;
 let calculatorBtn;
+let operator;
+let keepEvent = false;
 
-const calculatorDisplay = document.getElementById("Calculator-Display");
+const calculatorDisplay = document.getElementById("Display-top");
+const calculatorDisplayBot = document.getElementById("Display-bot");
 const clearBtn = document.getElementById("clear");
 const plusBtn = document.getElementById("addition");
 const minusBtn = document.getElementById("substraction");
 const multBtn = document.getElementById("multiplication");
 const divBtn = document.getElementById("division");
+const equalBtn = document.getElementById("equal");
 calculatorDisplay.textContent = x;
 
 
@@ -36,8 +47,8 @@ for(let i = 0; i < 10; i++){
     calculatorBtn = document.getElementById(`${i}`);
     calculatorBtn.addEventListener("click", () => {
         x = 10*x + i;
-        calculatorDisplay.textContent = x;
-        console.log(i);
+        keepEvent ? calculatorDisplay.textContent = `${y} ${operator} ${x}`: 
+        (calculatorDisplay.textContent = x);
     });
 
 }
@@ -46,12 +57,59 @@ for(let i = 0; i < 10; i++){
 
 clearBtn.addEventListener("click", () => {
     x = 0;
+    y = 0;
     calculatorDisplay.textContent = x;
+    calculatorDisplayBot.textContent = '';
 });
 
 //operator buttons
 
-// plusBtn.addEventListener("click", ()=> {
-    
-//     operate("+", x, y);
-// });
+plusBtn.addEventListener("click", ()=> {  
+    if(keepEvent){
+        keepEvent = false;
+        x = operate(operator, x, y)
+    }
+    operator = "+";
+    y = x;
+    x = 0;
+    keepEvent = true;
+    calculatorDisplay.textContent = `${y} ${operator} ${x}`
+});
+minusBtn.addEventListener("click", ()=> {  
+    if(keepEvent){
+        keepEvent = false;
+        x = operate(operator, x, y)
+    }
+    operator = "-";
+    y = x;
+    x = 0;
+    keepEvent = true;
+    calculatorDisplay.textContent = `${y} ${operator} ${x}`
+});
+multBtn.addEventListener("click", ()=> {  
+    if(keepEvent){
+        keepEvent = false;
+        x = operate(operator, x, y)
+    }
+    operator = "*";
+    y = x;
+    x = 0;
+    keepEvent = true;
+    calculatorDisplay.textContent = `${y} ${operator} ${x}`
+});
+divBtn.addEventListener("click", ()=> {  
+    if(keepEvent){
+        keepEvent = false;
+        x = operate(operator, x, y)
+    }
+    operator = "/";
+    y = x;
+    x = 0;
+    keepEvent = true;
+    calculatorDisplay.textContent = `${y} ${operator} ${x}`
+});
+
+equalBtn.addEventListener("click", ()=> {  
+    keepEvent = false;
+    x = operate(operator, x, y)
+});
